b5477d2a26afc5dd1ca4a58c56af860ada374fb8
Baptista-Paulo-Joaquim <baptistajoaquim0@gmail.com>
2025-02-05 15:19:56 +0200

    Cleaning code

--- server.js
+++ server.js
@@ -9,27 +9,42 @@ const PORT = 5000;
 app.use(cors());
 
 //Fetches users
-app.get("/read_users", async (req, res) => {
+app.get("/read", async (req, res) => {
     try {
         const allUsers = await prisma.user.findMany();
-        res.status(200).json({ message: "Users retrieved successfully", users: allUsers });
+        res.status(200).json({ message: "Users retrieved successfully"});
     } catch (error) {
         console.error("Error retrieving users:", error);
         res.status(500).json({ error: "Failed to retrieve users." });
     }
 });
 
-//Create user
-app.post("/create_user", async (req, res) => {
+//See user
+app.get("/user/:id", async (req, res) => {
+    let id = req.params.id;
+
     try {
+        const newUser = await prisma.user.findFirst({
+            where : { id: Number(id) } 
+        });
+        
+        console.log("Found!", newUser);
+        res.status(201).json({ message: "User found successfully!" });
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ error: "Failed to find a user." });
+    }
+});
+
+//Create user
+app.post("/create/user", async (req, res) => {
+    try {
+        let { name, email } = req.body;
         const newUser = await prisma.user.create({
-            data: {
-                name: "Catarina",
-                email: "catarinabola@gmail.com"
-            }
+            data: { name, email }
         });
         console.log("User Created!", newUser);
-        res.status(201).json({ message: "User created successfully!", user: newUser });
+        res.status(201).json({ message: "User created successfully!" });
     } catch (error) {
         console.error("Error creating user:", error);
         res.status(500).json({ error: "Failed to create user." });
@@ -37,13 +52,13 @@ app.post("/create_user", async (req, res) => {
 });
 
 //DELETES A USER
-app.delete("/delete_user/:id", async (req, res) => {
+app.delete("/delete/:id", async (req, res) => {
     const { id } = req.params;
     try {
         const deletedUser = await prisma.user.delete({
             where: { id: 2 },
         });
-        res.status(200).json({ message: "User deleted successfully", user: deletedUser });
+        res.status(200).json({ message: "User deleted successfully", });
     } catch (error) {
         console.error("Error deleting user:", error);
         res.status(500).json({ error: "Failed to delete user." });
@@ -51,7 +66,7 @@ app.delete("/delete_user/:id", async (req, res) => {
 });
 
 //UPDATES USER
-app.put("/update_user/:id", async (req, res) => {
+app.put("/update/:id", async (req, res) => {
     const { id } = req.params;
     try {
         const updatedUser = await prisma.user.update({
@@ -61,14 +76,12 @@ app.put("/update_user/:id", async (req, res) => {
                     email: "liaassanejacole@gmail.com"
                 }
         });
-        res.status(200).json({ message: "User updated successfully", user: updatedUser });
+        res.status(200).json({ message: "User updated successfully", });
     } catch (error) {
         console.error("Error updating user:", error);
         res.status(500).json({ error: "Failed to update user." });
     }
 });
-
-
 
 app.listen(PORT, (error) => {
     if(error) return error;
